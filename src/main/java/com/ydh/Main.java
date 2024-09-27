5b288708b8b0e8316804030f1b44a7b64dc76165
daehyeokyou <ydh4085@naver.com>
2024-09-27 14:28:15 +0900

    Leetcode Problem 79
    https://leetcode.com/problems/word-search/description/

--- src/main/java/com/ydh/Main.java
+++ src/main/java/com/ydh/Main.java
@@ -1,15 +1,23 @@
 package com.ydh;
 
 import com.ydh.problem.Solution1296;
+import com.ydh.problem.Solution79;
 
 public class Main {
     public static void main(String[] args) {
         // Solution 1296
-        int[] nums = {1,2,3,3,4,4,5,6};
-        int k=4;
-        Solution1296 sol = new Solution1296();
-        boolean result = sol.isPossibleDivide(nums, k);
-        System.out.println("result = " + result);
+//        int[] nums = {1,2,3,3,4,4,5,6};
+//        int k=4;
+//        boolean result1296 = new Solution1296().isPossibleDivide(nums, k);
+//        System.out.println("result = " + result1296);
 
+        char[][] board = {
+                {'C','A','A'},
+                {'A','A','A'},
+                {'B','C','D'}
+        };;
+        String word = "AAB";
+        Boolean result79 = new Solution79().exist(board, word);
+        System.out.println("result = " + result79);
     }
 }
---
+++ src/main/java/com/ydh/problem/Solution79.java
@@ -0,0 +1,40 @@
+package com.ydh.problem;
+
+public class Solution79 {
+
+    public boolean exist(char[][] board, String word) {
+        int m = board.length;
+        int n = board[0].length;
+
+        for(int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if(search(board, word, i, j, 0))
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean search(char[][] board, String word, int m, int n, int index) {
+        if(index == word.length()) {
+            return true;
+        }
+
+        if(m < 0 || board.length <= m || n < 0 || board[0].length <= n)
+            return false;
+
+        char findChar = word.charAt(index);
+        if(board[m][n] != findChar)
+            return false;
+
+        board[m][n] = '#';  // check
+        boolean found =
+                search(board, word, m+1, n, index+1) ||
+                search(board, word, m-1, n, index+1) ||
+                search(board, word, m, n+1, index+1) ||
+                search(board, word, m, n-1, index+1);
+
+        board[m][n] = findChar; // backtrack
+        return found;
+    }
+}
