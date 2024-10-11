f13744573707c572af78c47c5779a07969fd55f2
daehyeokyou <ydh4085@naver.com>
2024-10-11 16:54:47 +0900

    leetcode 1. Two-Sum

--- src/main/java/com/ydh/Main.java
+++ src/main/java/com/ydh/Main.java
@@ -1,5 +1,8 @@
 package com.ydh;
 
+import java.util.Arrays;
+
+import com.ydh.problem.Solution1;
 import com.ydh.problem.Solution114;
 import com.ydh.problem.Solution1296;
 import com.ydh.problem.Solution79;
@@ -25,19 +28,27 @@ public class Main {
         // Solution 114
         //    Input: root = [1,2,5,3,4,null,6]
         //    Output: [1 2 3 4 5 6 ]
-        Solution114.TreeNode root = new Solution114.TreeNode(1);
-        root.left = new Solution114.TreeNode(2);
-        root.left.left = new Solution114.TreeNode(3);
-        root.left.right = new Solution114.TreeNode(4);
-        root.right = new Solution114.TreeNode(5);
-        root.right.right = new Solution114.TreeNode(6);
+//        Solution114.TreeNode root = new Solution114.TreeNode(1);
+//        root.left = new Solution114.TreeNode(2);
+//        root.left.left = new Solution114.TreeNode(3);
+//        root.left.right = new Solution114.TreeNode(4);
+//        root.right = new Solution114.TreeNode(5);
+//        root.right.right = new Solution114.TreeNode(6);
+//
+//        new Solution114().flatten(root);
+//        System.out.print("[");
+//        while (root != null) {
+//            System.out.print(root.val + " ");
+//            root = root.right;
+//        }
+//        System.out.println("]");
 
-        new Solution114().flatten(root);
-        System.out.print("[");
-        while (root != null) {
-            System.out.print(root.val + " ");
-            root = root.right;
-        }
-        System.out.println("]");
+        // Solution 1
+        // Input: nums = [2,7,11,15], target = 9
+        // Output: [0,1]
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = new Solution1().twoSum(nums, target);
+        Arrays.stream(result).forEach(System.out::println);
     }
 }
