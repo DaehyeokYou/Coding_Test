b5c5151a37bbcd0c0ff7dc51071a5b1285494d29
daehyeokyou <ydh4085@naver.com>
2024-10-31 21:36:08 +0900

    main

--- src/main/java/com/ydh/Main.java
+++ src/main/java/com/ydh/Main.java
@@ -9,6 +9,8 @@ import java.util.List;
 import com.ydh.problem.Solution1;
 import com.ydh.problem.Solution114;
 import com.ydh.problem.Solution1296;
+import com.ydh.problem.Solution234;
+import com.ydh.problem.Solution78;
 import com.ydh.problem.Solution79;
 
 public class Main {
@@ -55,63 +57,66 @@ public class Main {
 //        int[] result = new Solution1().twoSum(nums, target);
 //        Arrays.stream(result).forEach(System.out::println);
 
-        // 정적 배열
-        /*
-        메서드가 없음.. add, remove가 껄끄러움... 사이즈가 미리 정해져야 함.
-         */
-        int[] staticArr = new int[5];
-        int[] staticArr2 = {1,2,3,4,5};
-        for(int v: staticArr) System.out.print(v + " ");    // 0 0 0 0 0
-        for(int v: staticArr2) System.out.print(v + " ");    // 1 2 3 4 5
-        // 리스트
-        /*
-        메서드가 있음!! add, remove가 쉬움!!!
-         */
-        List<Integer> list = new ArrayList<>();
-        ArrayList<Integer> arrayList = new ArrayList<>();
-
-
-        Integer[] staticArrBoxed = {3,4,1,7,9,2,0};
-//        Arrays.sort(staticArr2, Collections.reverseOrder());  // 에러
-        Arrays.sort(staticArrBoxed, Collections.reverseOrder());
-        for(int v: staticArrBoxed) System.out.print(v + " ");   // 9 7 4 3 2 1 0
-        Arrays.sort(staticArrBoxed);
-        for(int v: staticArrBoxed) System.out.print(v + " ");   // 0 1 2 3 4 7 9
-
-//        Arrays.sort(list);  // 에러
-        Collections.sort(list); // OK
-
-        Pair[] a = {
-                new Pair(1,8),
-                new Pair(1,6),
-                new Pair(1,9),
-                new Pair(5,7),
-                new Pair(3,4),
-                new Pair(1,4),
-                new Pair(6,4),
-                new Pair(9,0)
-        };
-        List<Pair> b = new ArrayList<>(Arrays.asList(new Pair(1,8), new Pair(5,7)));
-        b.add(new Pair(3, 5));
-        b.add(new Pair(1, 2));
-        b.add(new Pair(3, 3));
-        b.add(new Pair(2, 4));
-        System.out.println();
-        // 1순위는 y는 오름차순, 2순위는 x는 내림차순
-        Arrays.sort(a, (p1,p2) -> (p1.y == p2.y) ? Integer.compare(p2.x, p1.x) : Integer.compare(p1.y, p2.y));
-        for(Pair p: a) System.out.println(p.x + ", " + p.y);
-
-        // 1순위 x는 오름차순, 2순위 y는 내림차순
-        Collections.sort(b, (p1, p2) -> (p1.x == p2.x) ? Integer.compare(p2.y, p1.y) : Integer.compare(p1.x, p2.x));
-        for(Pair p: b) System.out.println(p.x + ", " + p.y);
+//        // 정적 배열
+//        /*
+//        메서드가 없음.. add, remove가 껄끄러움... 사이즈가 미리 정해져야 함.
+//         */
+//        int[] staticArr = new int[5];
+//        int[] staticArr2 = {1,2,3,4,5};
+//        for(int v: staticArr) System.out.print(v + " ");    // 0 0 0 0 0
+//        for(int v: staticArr2) System.out.print(v + " ");    // 1 2 3 4 5
+//        // 리스트
+//        /*
+//        메서드가 있음!! add, remove가 쉬움!!!
+//         */
+//        List<Integer> list = new ArrayList<>();
+//        ArrayList<Integer> arrayList = new ArrayList<>();
+//
+//
+//        Integer[] staticArrBoxed = {3,4,1,7,9,2,0};
+////        Arrays.sort(staticArr2, Collections.reverseOrder());  // 에러
+//        Arrays.sort(staticArrBoxed, Collections.reverseOrder());
+//        for(int v: staticArrBoxed) System.out.print(v + " ");   // 9 7 4 3 2 1 0
+//        Arrays.sort(staticArrBoxed);
+//        for(int v: staticArrBoxed) System.out.print(v + " ");   // 0 1 2 3 4 7 9
+//
+////        Arrays.sort(list);  // 에러
+//        Collections.sort(list); // OK
+//
+//        Pair[] a = {
+//                new Pair(1,8),
+//                new Pair(1,6),
+//                new Pair(1,9),
+//                new Pair(5,7),
+//                new Pair(3,4),
+//                new Pair(1,4),
+//                new Pair(6,4),
+//                new Pair(9,0)
+//        };
+//        List<Pair> b = new ArrayList<>(Arrays.asList(new Pair(1,8), new Pair(5,7)));
+//        b.add(new Pair(3, 5));
+//        b.add(new Pair(1, 2));
+//        b.add(new Pair(3, 3));
+//        b.add(new Pair(2, 4));
+//        System.out.println();
+//        // 1순위는 y는 오름차순, 2순위는 x는 내림차순
+//        Arrays.sort(a, (p1,p2) -> (p1.y == p2.y) ? Integer.compare(p2.x, p1.x) : Integer.compare(p1.y, p2.y));
+//        for(Pair p: a) System.out.println(p.x + ", " + p.y);
+//
+//        // 1순위 x는 오름차순, 2순위 y는 내림차순
+//        Collections.sort(b, (p1, p2) -> (p1.x == p2.x) ? Integer.compare(p2.y, p1.y) : Integer.compare(p1.x, p2.x));
+//        for(Pair p: b) System.out.println(p.x + ", " + p.y);
+        Solution78 sol = new Solution78();
+        int nums[] = {3, 2, 4, 1};
+        sol.subsets(nums);
 
     }
-    public static class Pair {
-        int x;
-        int y;
-        public Pair(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
+//    public static class Pair {
+//        int x;
+//        int y;
+//        public Pair(int x, int y) {
+//            this.x = x;
+//            this.y = y;
+//        }
+//    }
 }
